d5966f30d4a71a5328624b9b89737f6ca1812019
lqyrmk <1638574981@qq.com>
2026-04-20 17:57:38 +0800

    chore: refactor KMeans and modify some variable names

--- tear/KMeans/KMeans_numpy.py
+++ tear/KMeans/KMeans_numpy.py
@@ -5,7 +5,7 @@ class KMeans:
     def __init__(self, k=3, max_iters=100, threshold=1e-4, random_state=None):
         self.k = k
         self.max_iters = max_iters
-        self.threshold = threshold
+        self.tol = tol  # tolerance
         self.random_state = random_state
 
     def fit(self, x):
@@ -14,36 +14,36 @@ class KMeans:
         return KMeans object
         """
 
-        N, D = x.shape
+        N, _ = x.shape
         np.random.seed(self.random_state)
 
         # 初始化 K 个随机质心
-        random_center_indices = np.random.choice(N, self.k, replace=False)  # 不放回
-        self.centers = x[random_center_indices]  # [K, D]
+        random_centroid_indices = np.random.choice(N, self.k, replace=False)  # 不放回
+        self.centroids = x[random_centroid_indices]  # [K, D]
 
         for _ in range(self.max_iters):
             # 计算每个点与质心的距离
-            distances = self.L2_distance(x, self.centers)  # [N, K]
+            distances = self.L2_distance(x, self.centroids)  # [N, K]
 
             # 根据距离分配簇
             labels = np.argmin(distances, axis=1)  # [N,]
 
             # 更新中心
-            new_centers = np.zeros_like(self.centers)  # [K, D]
+            new_centroids = np.zeros_like(self.centroids)  # [K, D]
             for i in range(self.k):
                 cluster_x = x[labels == i]  # [Mi, D]
                 if len(cluster_x) > 0:
-                    new_centers[i] = cluster_x.mean(axis=0)
+                    new_centroids[i] = cluster_x.mean(axis=0)
                 else:  # 空簇
                     # 1.继承之前的簇中心
-                    # new_centers[i] = self.centers[i]
+                    # new_centroids[i] = self.centroids[i]
                     # 2.随机选取
-                    new_centers[i] = x[np.random.randint(0, N)]
+                    new_centroids[i] = x[np.random.randint(0, N)]
 
-            if np.linalg.norm(self.centers - new_centers) < self.threshold:
+            if np.linalg.norm(self.centroids - new_centroids) < self.tol:
                 break
 
-            self.centers = new_centers
+            self.centroids = new_centroids
 
         self.labels = labels
         return self
@@ -67,7 +67,7 @@ class KMeans:
         x: [N, K]
         return label: [N,]
         """
-        dis = self.L2_distance(x, self.centers)  # [N, K]
+        dis = self.L2_distance(x, self.centroids)  # [N, K]
         pred_label = np.argmin(dis, axis=1)  # [N,]
         return pred_label
 
@@ -76,16 +76,16 @@ if __name__ == "__main__":
     seed = 42
     k = 4
     max_iters = 100
-    threshold = 1e-4
+    tol = 1e-4
     N = 20
     D = 10
     x = np.random.rand(N, D)  # 均匀分布
 
     # 训练
-    model = KMeans(k, max_iters, threshold, seed)
+    model = KMeans(k, max_iters, tol, seed)
     model.fit(x)
 
-    print(f"centers: {model.centers}")
+    print(f"centroids: {model.centroids}")
 
     # 预测
     test_N = 5
